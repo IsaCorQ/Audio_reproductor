5903187ef1cb67991f29241f66310d2723f99b0e
IsaCorQ <isabelcordobaquesada@gmail.com>
2025-05-29 10:21:13 -0600

    Cambio al diseño

--- Audioempotrados/audioplay/audioplay_inst.v
+++ Audioempotrados/audioplay/audioplay_inst.v
@@ -1,5 +1,28 @@
 	audioplay u0 (
-		.clk_clk       (<connected-to-clk_clk>),       //   clk.clk
-		.reset_reset_n (<connected-to-reset_reset_n>)  // reset.reset_n
+		.clk_clk            (<connected-to-clk_clk>),            //        clk.clk
+		.filtro1_sw_export  (<connected-to-filtro1_sw_export>),  // filtro1_sw.export
+		.filtro2_sw_export  (<connected-to-filtro2_sw_export>),  // filtro2_sw.export
+		.memory_mem_a       (<connected-to-memory_mem_a>),       //     memory.mem_a
+		.memory_mem_ba      (<connected-to-memory_mem_ba>),      //           .mem_ba
+		.memory_mem_ck      (<connected-to-memory_mem_ck>),      //           .mem_ck
+		.memory_mem_ck_n    (<connected-to-memory_mem_ck_n>),    //           .mem_ck_n
+		.memory_mem_cke     (<connected-to-memory_mem_cke>),     //           .mem_cke
+		.memory_mem_cs_n    (<connected-to-memory_mem_cs_n>),    //           .mem_cs_n
+		.memory_mem_ras_n   (<connected-to-memory_mem_ras_n>),   //           .mem_ras_n
+		.memory_mem_cas_n   (<connected-to-memory_mem_cas_n>),   //           .mem_cas_n
+		.memory_mem_we_n    (<connected-to-memory_mem_we_n>),    //           .mem_we_n
+		.memory_mem_reset_n (<connected-to-memory_mem_reset_n>), //           .mem_reset_n
+		.memory_mem_dq      (<connected-to-memory_mem_dq>),      //           .mem_dq
+		.memory_mem_dqs     (<connected-to-memory_mem_dqs>),     //           .mem_dqs
+		.memory_mem_dqs_n   (<connected-to-memory_mem_dqs_n>),   //           .mem_dqs_n
+		.memory_mem_odt     (<connected-to-memory_mem_odt>),     //           .mem_odt
+		.memory_mem_dm      (<connected-to-memory_mem_dm>),      //           .mem_dm
+		.memory_oct_rzqin   (<connected-to-memory_oct_rzqin>),   //           .oct_rzqin
+		.min1_export        (<connected-to-min1_export>),        //       min1.export
+		.min2_export        (<connected-to-min2_export>),        //       min2.export
+		.reset_reset_n      (<connected-to-reset_reset_n>),      //      reset.reset_n
+		.seg1_export        (<connected-to-seg1_export>),        //       seg1.export
+		.seg2_export        (<connected-to-seg2_export>),        //       seg2.export
+		.rst_export         (<connected-to-rst_export>)          //        rst.export
 	);
 
